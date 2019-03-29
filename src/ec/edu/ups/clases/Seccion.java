12e2362d383e735194f2fac56c370b2a85548cde
Edison <Edison@172.16.210.34>
2019-03-29 12:42:20 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/clases/Seccion.java
+++ src/ec/edu/ups/clases/Seccion.java
@@ -1,6 +1,12 @@
 
 package ec.edu.ups.clases;
-
+/**
+ * EstructuraPeriodico
+ * Calase que permite estructurar mi periodici digital
+ * @version 2.0
+ * @since  2019
+ * @author Edison
+ */
 
 public class Seccion {
     
