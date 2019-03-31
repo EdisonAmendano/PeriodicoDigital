04315f72f859e3679529185d460ce6b1ae92341b
Edison <Edison@172.16.210.34>
2019-03-31 17:01:49 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/clases/Noticia.java
+++ src/ec/edu/ups/clases/Noticia.java
@@ -6,6 +6,13 @@ import java.util.Date;
 
 public class Noticia {
     
+    /**
+    * Noticia
+    * Calase que permite editar(crear, modificar, eiminar, etc.) las noticias del periodeico digital
+    * @since  2019
+    * @author Edison
+    */
+    
     private String titulo;
     private String autor;
     private Date fecha;
@@ -13,34 +20,82 @@ public class Noticia {
     private Multimedia multimedia;
 
     public String getTitulo() {
+        /**
+        * getTitulo
+        * Metodo que me permite obtener el titulo de la noticia
+        * @since  2019
+        * @author Edison
+        */
         return titulo;
     }
 
     public void setTitulo(String titulo) {
+        /**
+        * setTitulo
+        * Metodo que me permite establecer el nombre de la seccion
+        * @since  2019
+        * @author Edison
+        */
         this.titulo = titulo;
     }
 
     public String getAutor() {
+        /**
+        * getAutor
+        * Metodo que me permite obtener el Autor de la noticia
+        * @since  2019
+        * @author Edison
+        */
         return autor;
     }
 
     public void setAutor(String autor) {
+        /**
+        * setAutor
+        * Metodo que me permite establecer el Autor de la noticia
+        * @since  2019
+        * @author Edison
+        */
         this.autor = autor;
     }
 
     public Date getFecha() {
+        /**
+        * getFecha
+        * Metodo que me permite obtener la fecha de publicación de la noticia
+        * @since  2019
+        * @author Edison
+        */
         return fecha;
     }
 
     public void setFecha(Date fecha) {
+        /**
+        * setFecha
+        * Metodo que me permite establecer la fecha de publicación de la noticia
+        * @since  2019
+        * @author Edison
+        */
         this.fecha = fecha;
     }
 
     public String getContenido() {
+        /**
+        * getContenido
+        * Metodo que me permite obtener el contenido de la noticia
+        * @since  2019
+        * @author Edison
+        */
         return contenido;
     }
 
     public void setContenido(String contenido) {
+        /**
+        * setContenido
+        * Metodo que me permite establecer el contenido de la noticia
+        * @since  2019
+        * @author Edison
+        */
         this.contenido = contenido;
     }
     
